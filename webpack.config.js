709d6b12f8906d8a24a4baacc2e5ada815ded82b
nyaliti <bnyaliti@gmail.com>
2023-05-13 21:52:05 +0300

    new updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path'); // eslint-disable-line no-unused-vars
 
 module.exports = {
+  // ... other webpack configuration options
   resolve: {
     fallback: {
       path: require.resolve('path-browserify'),
       net: require.resolve('net-browserify'),
+      fs: require.resolve('graceful-fs'),
     },
   },
 };
