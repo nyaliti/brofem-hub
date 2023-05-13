4a4990faf8597ecf01ce96428d1a61cd52e9d9c8
nyaliti <bnyaliti@gmail.com>
2023-05-13 21:16:53 +0300

    disabled ESLint rule for unused vars

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path'); // eslint-disable-line no-unused-vars
+
 module.exports = {
   resolve: {
     fallback: {
+      'path': require.resolve('path-browserify'),
       'util': require.resolve('util/'),
       'url': require.resolve('url/'),
     },
   },
 };
-
