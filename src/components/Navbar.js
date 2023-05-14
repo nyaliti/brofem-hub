3f90c5803f261c821901371a2c91136127b68c92
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-14 17:52:37 +0300

    updated Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Register</li>
+        <li>Create Post</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
