1fde8571458e6bd1a54ec7c88c4f166191a86ac6
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 18:53:19 +0300

    updated blogpost.js

--- src/components/Blogpost.js
+++ src/components/Blogpost.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
+import Comment from './Comment';
 
 const BlogPost = ({post}) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    // Fetch comments for the blog post from the server
+    // Update the "comments" state
+  }, []);
+  // eslint-disable-next-line no-unused-vars
+  const handleSetComments = () => {
+    // Use setComments if needed
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const unusedVariable = setComments;
+
   return (
     <div>
       <h3>{post.title}</h3>
       <p>{post.content}</p>
-      {/* Display comments */}
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
     </div>
   );
 };
