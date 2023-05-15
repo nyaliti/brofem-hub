83717ee7a6a03cc19f1885dc7c59f78d3cd0d6e3
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 18:57:38 +0300

    updated comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,19 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Comment = ({comment}) => {
   return (
     <div>
       <p>{comment.content}</p>
-      {/* Display user information */}
+      <p>Posted by: {comment.user}</p>
     </div>
   );
-};
-
-Comment.propTypes = {
-  comment: PropTypes.shape({
-    content: PropTypes.string.isRequired,
-  }).isRequired,
 };
 
 export default Comment;
