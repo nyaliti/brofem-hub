d12adeae7e260c6e53bc3a2698c1176a3a0edcde
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 20:28:16 +0300

    created Contactus.js

---
+++ src/components/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      <h3>Contact Us</h3>
+      <p>For any inquiries or support, please reach out to us:</p>
+      <ul>
+        <li>Email: contact@example.com</li>
+        <li>Phone: 123-456-7890</li>
+        <li>Address: 123 Street, City, Country</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactUs;
