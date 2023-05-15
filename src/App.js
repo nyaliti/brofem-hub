7ea7b9943b1a2c4e7e73d996cde3a7a2b9eb5215
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 19:38:22 +0300

    updated app.js but with switch importation errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import CreatePost from './components/CreatePost';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <Navbar />
-        <Home />
-        {/* Other components */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/create-post" component={CreatePost} />
+        </Switch>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 };
 
