cd8d6ff6eecf29de70703cb3b20ae8e37912d551
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 20:20:31 +0300

    created footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <div className="container text-center">
+        <p>&copy; 2023 Your Blog. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
