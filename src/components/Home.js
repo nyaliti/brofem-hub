8fd65bfabe1652efbac575fbb4bc44b4417b79ca
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 18:39:35 +0300

    Updated the Home.js to fetch and display blog post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import BlogPost from './BlogPost';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch blog posts from the server
+    // Update the "posts" state
+  }, []);
+
+  // eslint-disable-next-line no-unused-vars
+  const handleSetPosts = () => {
+    setPosts([...posts, {id: 1, title: 'New Post', content: 'Lorem ipsum'}]);
+  };
+
   return (
     <div>
       <h2>Welcome to the Blogging Platform</h2>
-      {/* Display blog posts */}
+      {posts.map((post) => (
+        <BlogPost key={post.id} post={post} />
+      ))}
     </div>
   );
 };
