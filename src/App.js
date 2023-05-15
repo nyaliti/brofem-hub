111d5fa648abf0394928f598ce22983573f50e16
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 19:18:11 +0300

    made several corrections relating react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-/* eslint-disable max-len */
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      {/* Other components */}
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Home />
+        {/* Other components */}
+      </div>
+    </BrowserRouter>
   );
 };
 
