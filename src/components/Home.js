1035ad75773d5140363a817e124303ea732963d6
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 18:13:02 +0300

    created Blogpost.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Post from './Post';
 
 const Home = () => {
-  // Sample data for blog posts
-  const posts = [
-    {id: 1, title: 'First Post', content: 'This is the first blog post.'},
-    {id: 2, title: 'Second Post', content: 'This is the second blog post.'},
-    // Add more sample posts
-  ];
-
   return (
     <div>
-      <h2>Recent Blog Posts</h2>
-      {posts.map((post) => (
-        <Post key={post.id} title={post.title} content={post.content} />
-      ))}
+      <h2>Welcome to the Blogging Platform</h2>
+      {/* Display blog posts */}
     </div>
   );
 };
