a53d6ed149c4177344ac1f2de28469b0069ff1cb
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-15 20:27:29 +0300

    edited footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-light py-3">
       <div className="container text-center">
-        <p>&copy; 2023 Your Blog. All rights reserved.</p>
+        <p>&copy; 2023 Brofem club. All rights reserved.</p>
       </div>
     </footer>
   );
