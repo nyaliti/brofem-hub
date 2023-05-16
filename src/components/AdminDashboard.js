04d9e038f2a7a2a426fc9330d652777b0dbfb681
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-16 15:05:51 +0300

    created admindashboard

---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+  // Implement admin-specific functionalities
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      {/* Add admin-specific components and functionalities */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
