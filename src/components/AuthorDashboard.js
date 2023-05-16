93834f4419c7377bf8507b73ddd4f8a482658818
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-16 15:04:18 +0300

    updated author

---
+++ src/components/AuthorDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AuthorDashboard = () => {
+  // Implement author-specific functionalities
+  return (
+    <div>
+      <h2>Author Dashboard</h2>
+      {/* Add author-specific components and functionalities */}
+    </div>
+  );
+};
+
+export default AuthorDashboard;
+
+
