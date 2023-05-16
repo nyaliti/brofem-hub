baf381cfc361572692db4ed25fcfd87dc92026df
bryson <65848252+nyaliti@users.noreply.github.com>
2023-05-16 14:36:53 +0300

    updated naigation link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,45 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom/cjs/react-router-dom.min';
 
-const Navbar = () => {
+
+// eslint-disable-next-line react/prop-types
+const Navbar = ({isAuthenticated, userRole, handleLogout}) => {
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/create-post">Create Post</Link>
-        </li>
+        {isAuthenticated && userRole === 'user' && (
+          <li>
+            <Link to="/comment">comment</Link>
+          </li>
+        )}
+        {isAuthenticated && userRole === 'author' && (
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        )}
+        {isAuthenticated && userRole === 'admin' && (
+          <li>
+            <Link to="/admin">Admin Dashboard</Link>
+          </li>
+        )}
+        {!isAuthenticated && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
+        {isAuthenticated && (
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        )}
       </ul>
     </nav>
   );
